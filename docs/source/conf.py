805ef0ed6ea9a83e04826da2baffa36878df7c94
Alexander Kmoch <alexander.kmoch@ut.ee>
2025-06-27 14:49:53 +0200

    starting readthedocs #7

---
+++ docs/source/conf.py
@@ -0,0 +1,66 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
+
+project = 'pydggsapi'
+copyright = '2025, Wai Tik Chan, Alexander Kmoch'
+author = 'Wai Tik Chan, Alexander Kmoch'
+release = '2025'
+
+release = '0.1.2'
+version = '0.1.2'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'myst_parser'
+]
+
+exclude_patterns = []
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+
+# Configuration file for the Sphinx documentation builder.
+
+
+
+
+
+# -- General configuration
+
+
+
+
+
+# -- Options for HTML output
+
+
+
+# -- Options for EPUB output
