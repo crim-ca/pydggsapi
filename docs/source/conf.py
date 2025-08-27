fb3cc50f97a6f167a441cbfd6047783ec1d17fe6
Alexander Kmoch <alexander.kmoch@ut.ee>
2025-08-27 18:29:55 +0300

    mini clean up of docs conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,22 +45,3 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
-
-# Configuration file for the Sphinx documentation builder.
-
-
-
-
-
-# -- General configuration
-
-
-
-
-
-# -- Options for HTML output
-
-
-
-# -- Options for EPUB output
