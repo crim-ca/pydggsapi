17b02bc25b0e7b641c9fedf755d411a200ea1d73
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2025-10-23 17:29:17 -0400

    add schema classes for dggrs definition

--- pydggsapi/schemas/ogc_dggs/dggrs_descrption.py
+++ pydggsapi/schemas/ogc_dggs/dggrs_descrption.py
@@ -1,13 +1,209 @@
 from __future__ import annotations
 from pydggsapi.schemas.ogc_dggs.common_ogc_dggs_api import Link, LinkTemplate, CrsModel
-from typing import List, Optional, Union
-from pydantic import AnyUrl, BaseModel, Field, conint, model_validator
+from typing import List, Literal, Optional, Union, get_args
+from pydantic import AnyHttpUrl, AnyUrl, BaseModel, Field, conint, model_validator
 from fastapi import HTTPException
+
+DgghZoneType = Literal[
+    "triangle",
+    "square",
+    "hexagon",
+    "pentagon",
+    "rhombus"
+]
+DgghRefinementStrategy = Literal[
+    "centredChildCell",
+    "nestedChildCell",
+    "nodeCentredChildCell",
+    "nodeSharingChildCell",
+    "edgeCentredChildCell",
+    "faceCentredChildCell",
+    "solidCentredChildCell"
+]
+ZirType = Literal[
+    "hierarchicalConcatenation",
+    "ogc2DTMSHexLevelRowCol",
+    "levelRootFaceHexRowMajorSubZone"
+]
+SubZoneOrderType = Literal[
+    "scanline",
+    "spiralFromCenter",
+    "mortonCurve",
+    "hilbertCurve"
+]
 
 
 class DggrsDescriptionRequest(BaseModel):
     dggrsId: str  # = Path(...)
     collectionId: Optional[str] = None
+
+
+class DgghConstraints(BaseModel):
+    cellAxisAligned: Optional[bool] = Field(
+        default=False,
+        description=(
+            "Set to true if all edges of the geometry of all zones are aligned with one of the axis of the 'crs'."
+        ),
+    ),
+    cellConformal: Optional[bool] = Field(default=False)
+    cellEquiAngular: Optional[bool] = Field(default=False)
+    cellEquiDistant: Optional[bool] = Field(default=False)
+    cellEqualSized: Optional[bool] = Field(
+        default=False,
+        description=(
+            "Set to true if the area of all zones is the same for a particular zone geometry type of any "
+            "specific discrete global grid of the DGG hierarchy."
+        ),
+    )
+
+
+class DgghOrientation(BaseModel):
+    # use DGGRID defaults for backward compatibility of reported values if not overridden
+    # otherwise, it is STRONGLY recommended to override with reference DGGRS definitions
+    # see: https://docs.ogc.org/DRAFTS/21-038r1.html#annex-dggrs-def
+    latitude: Union[float, int] = Field(
+        default=58.28252559,
+        description="Reference geodetic Latitude in decimal degrees to fix the orientation of the polyhedron.",
+        examples=[58.397145907431],
+    )
+    longitude: Union[float, int] = Field(
+        default=11.25,
+        description="Reference Longitude in decimal degrees to fix the orientation of the first vertex.",
+        examples=[11.20],
+    )
+    azimuth: Optional[Union[float, int]] = Field(
+        default=0.0,
+        description="Azimuth in decimal degrees of second vertex relative to the first vertex.",
+        examples=[0.0],
+    )
+
+
+class DgghParameters(BaseModel):
+    ellipsoid: Optional[Union[AnyHttpUrl, str]] = Field(
+        default=0.0,
+        description="Globe Reference System Identifier/Specification",
+        examples=["EPGS:7019"],
+    )
+    orientation: Optional[DgghOrientation] = None
+
+
+class DgghDefinition(BaseModel):
+    crs: Optional[CrsModel] = Field(
+        default=None,
+        description=(
+            "The native Coordinate Reference System (CRS) in which the geometry of "
+            "the zones for this DGG hierarchy is defined."
+        )
+    )
+    basePolyhedron: Optional[str] = Field(
+        default=None,
+        description=(
+            "The Type/Class of Polyhedron used to construct the Discrete Global Grid System "
+            "if it is constructed using a Base Polyhedron. "
+        ),
+        examples=["icosahedron"],
+
+    )
+    refinementRatio: Optional[int] = Field(
+        default=None,
+        description=(
+            "The ratio of the area of zones between two consecutive hierarchy level "
+            "(the ratio of child zones to parent zones, also called the aperture)."
+        ),
+        examples=[9],
+    )
+    constraints: Optional[DgghConstraints] = None
+    spatialDimensions: int = Field(
+        default=2,  # DGGRS that are supported in this API are all 2D coordinates
+        description=(
+            "Number of Spatial Dimensions defined by the Discrete Global Grid System. "
+            "This represents the number of spatial dimensions embedded within respective zone IDs, "
+            "to locate the specific zone. Typically, this would be 2 or 3 based on 2D or 3D coordinate systems, "
+            "but can represent other encoding strategies."
+        ),
+    )
+    # see: https://github.com/opengeospatial/ogcapi-discrete-global-grid-systems/issues/102
+    # see: https://docs.ogc.org/as/20-040r3/20-040r3.html#toc20
+    temporalDimensions: int = Field(
+        default=0,  # DGGRS that are supported in this API are all 0-dims temporal
+        description=(
+            "Number of Temporal Dimensions defined by the Discrete Global Grid System. "
+            "This represents temporal reference system natively embedded within respective zone IDs, "
+            "and not a DGGRS with temporal awareness accessed by other means than the zone IDs themselves "
+            "(e.g.: using 'datetime' query, CQL2, or another property). "
+        ),
+    )
+    zoneTypes: Optional[List[Union[DgghZoneType, str]]] = Field(
+        examples=list(get_args(DgghZoneType)),
+    )
+    refinementStrategy: Optional[List[DgghRefinementStrategy]] = Field(
+        description="The refinement strategy used by the Discrete Global Grid System",
+        examples=list(get_args(DgghRefinementStrategy)),
+    )
+    parameters: Optional[DgghParameters] = Field(
+        default=None,
+        description=(
+            "The optional parameters establishing a very specific Discrete Global Grid System, "
+            "where each zone has a well-defined geometry."
+        ),
+    )
+
+
+class DggrsHierarchy(BaseModel):
+    """
+    The hierarchical series of Discrete Global Grid upon which this DGGRS is based, including any parameters.
+    """
+    # see: https://docs.ogc.org/DRAFTS/21-038r1.html#annex-dggrs-def
+
+    definition: DgghDefinition
+
+
+class ZirDefinition(BaseModel):
+    description: str = Field(
+        description="Detailed human-readable description of the ZIR representation."
+    )
+    type: Optional[Union[str, ZirType]] = Field(
+        default=None,
+        examples=list(get_args(ZirType)),
+    )
+
+
+class DggrsZirs(BaseModel):
+    textZIRS: ZirDefinition = Field(
+        description="textual zone identifier indexing scheme",
+    )
+    uint64ZIRS: ZirDefinition = Field(
+        default="ogc2DTMSHexLevelRowCol",
+        description="64-bit unsigned integer zone indexing scheme",
+    )
+
+
+class DggrsSubZoneOrder(BaseModel):
+    description: str = Field(
+        description="Detailed human-readable description of the sub-zone ordering method."
+    )
+    type: Optional[Union[str, SubZoneOrderType]] = Field(
+        default=None,
+        examples=list(get_args(SubZoneOrderType)),
+    )
+
+
+class DggrsDefinition(BaseModel):
+    dggh: Optional[DggrsHierarchy] = Field(
+        default=None,
+        description="The hierarchical series of Discrete Global Grid upon which this DGGRS is based, including any parameters.",
+    )
+    zirs: Optional[DggrsZirs] = Field(
+        default=None,
+        description="The Zone Identifier Reference System used for this Discrete Global Grid System Reference System.",
+    )
+    subZoneOrder: Optional[DggrsSubZoneOrder] = Field(
+        default=None,
+        description=(
+            "The ordering used for this Discrete Global Grid System Reference System when encoding "
+            "the values associated with sub-zones at any given depth relative to a parent zone."
+        ),
+    )
 
 
 class DggrsDescription(BaseModel):
