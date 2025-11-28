f5df3fb23ae1698a7fb0812eb044e9276aa62d6d
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2025-11-28 00:21:05 -0500

    remove explict 'Extent' instanatiation, let pydantic auto-resolve/validate them

--- pydggsapi/dependencies/api/collections.py
+++ pydggsapi/dependencies/api/collections.py
@@ -1,6 +1,4 @@
 from pydggsapi.schemas.api.collections import Collection
-from pydggsapi.schemas.ogc_collections.collections import Extent
-from pydggsapi.schemas.ogc_collections.extent import Spatial, Temporal
 
 from tinydb import TinyDB
 import logging
@@ -21,10 +19,6 @@ def get_collections_info() -> dict[str, Collection]:
     collections_dict = {}
     for collection in collections:
         cid, collection_config = collection.popitem()
-        if (collection_config.get('extent') is not None):
-            spatial = Spatial(**collection_config['extent'].get('spatial', {}))
-            temporal = Temporal(**collection_config['extent']['temporal']) if collection_config['extent'].get('temporal') else None
-            collection_config['extent'] = Extent(spatial=spatial, temporal=temporal)
         collection_config['id'] = cid
         collections_dict[cid] = Collection(**collection_config)
     return collections_dict
